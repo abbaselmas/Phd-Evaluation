3b85a24d221da219e560475749fdb355602504d3
Abbas ELMAS <abbaselmas@gmail.com>
2025-02-25 15:14:02 -0300

    mobile timing results updated with new flann crosscheck true version

--- main.py
+++ main.py
@@ -1,9 +1,9 @@
 from synthetic import *
 from oxfordAffine import *
-# from drone import *
-# from uav import *
-# from display import *
-# from airsim import *
+from drone import *
+from uav import *
+from display import *
+from airsim import *
 
 a = 100
 b = 100
@@ -12,17 +12,17 @@ save = True
 reconstruct = False
 mobile = "_mobile"
 
-execute_scenario_rotation (a, b, drawing, save, mobile)
-execute_scenario_scale    (a, b, drawing, save, mobile)
-execute_scenario_intensity(a, b, drawing, save, mobile)
-executeScenarios          ("graf",   a, b, drawing, save, mobile)
-executeScenarios          ("leuven", a, b, drawing, save, mobile)
-executeScenarios          ("bark",   a, b, drawing, save, mobile)
-executeScenarios          ("boat",   a, b, drawing, save, mobile)
-executeScenarios          ("bikes",  a, b, drawing, save, mobile)
-executeScenarios          ("trees",  a, b, drawing, save, mobile)
-executeScenarios          ("wall",   a, b, drawing, save, mobile)
-executeScenarios          ("ubc",    a, b, drawing, save, mobile)
+# execute_scenario_rotation (a, b, drawing, save, mobile)
+# execute_scenario_scale    (a, b, drawing, save, mobile)
+# execute_scenario_intensity(a, b, drawing, save, mobile)
+# executeScenarios          ("graf",   a, b, drawing, save, mobile)
+# executeScenarios          ("leuven", a, b, drawing, save, mobile)
+# executeScenarios          ("bark",   a, b, drawing, save, mobile)
+# executeScenarios          ("boat",   a, b, drawing, save, mobile)
+# executeScenarios          ("bikes",  a, b, drawing, save, mobile)
+# executeScenarios          ("trees",  a, b, drawing, save, mobile)
+# executeScenarios          ("wall",   a, b, drawing, save, mobile)
+# executeScenarios          ("ubc",    a, b, drawing, save, mobile)
 # executeUAVScenarios       ("uav",    a, b, drawing, save, mobile)
 # executeAirSimScenarios    ("airsim", a, b, drawing, save, mobile)
 # executeDroneScenarios     ("drone",  a, b, drawing, save, mobile, reconstruct)
