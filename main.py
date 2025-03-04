57b3feab92db84c679f7912b129b30d5e60ff13b
Abbas ELMAS <abbaselmas@gmail.com>
2025-03-04 04:18:43 -0300

    run mobile for flann again

--- main.py
+++ main.py
@@ -1,28 +1,28 @@
 from synthetic import *
 from oxfordAffine import *
-from drone import *
-from uav import *
-from display import *
-from airsim import *
+# from drone import *
+# from uav import *
+# from display import *
+# from airsim import *
 
 a = 100
 b = 100
 drawing = False
 save = True
-reconstruct = True
-mobile = "" #"_mobile"
+reconstruct = False
+mobile = "_mobile"
 
-# execute_scenario_rotation (a, b, drawing, save, mobile)
-# execute_scenario_scale    (a, b, drawing, save, mobile)
-# execute_scenario_intensity(a, b, drawing, save, mobile)
-# executeScenarios          ("graf",   a, b, drawing, save, mobile)
-# executeScenarios          ("leuven", a, b, drawing, save, mobile)
-# executeScenarios          ("bark",   a, b, drawing, save, mobile)
-# executeScenarios          ("boat",   a, b, drawing, save, mobile)
-# executeScenarios          ("bikes",  a, b, drawing, save, mobile)
-# executeScenarios          ("trees",  a, b, drawing, save, mobile)
-# executeScenarios          ("wall",   a, b, drawing, save, mobile)
-# executeScenarios          ("ubc",    a, b, drawing, save, mobile)
+execute_scenario_rotation (a, b, drawing, save, mobile)
+execute_scenario_scale    (a, b, drawing, save, mobile)
+execute_scenario_intensity(a, b, drawing, save, mobile)
+executeScenarios          ("graf",   a, b, drawing, save, mobile)
+executeScenarios          ("leuven", a, b, drawing, save, mobile)
+executeScenarios          ("bark",   a, b, drawing, save, mobile)
+executeScenarios          ("boat",   a, b, drawing, save, mobile)
+executeScenarios          ("bikes",  a, b, drawing, save, mobile)
+executeScenarios          ("trees",  a, b, drawing, save, mobile)
+executeScenarios          ("wall",   a, b, drawing, save, mobile)
+executeScenarios          ("ubc",    a, b, drawing, save, mobile)
 # executeUAVScenarios       ("uav",    a, b, drawing, save, mobile)
 # executeAirSimScenarios    ("airsim", a, b, drawing, save, mobile)
 # executeDroneScenarios     ("drone",  a, b, drawing, save, mobile, reconstruct)
@@ -46,7 +46,7 @@ mobile = "" #"_mobile"
 # singleAll("drone")
 # single("drone")
 # singleTiming("drone")
-singleEfficiency("drone")
+# singleEfficiency("drone")
 # heatmap("drone")
 # correlationHeatmap("drone")
 # violinPlot("drone")
